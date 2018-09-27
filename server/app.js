fa9b90f2024d15d5a7a678d0c5d3713658250e85
Igor Amidzic <igoramidzic46@gmail.com>
2018-09-27 15:24:41 -0400

    feature/server: Igor - add basic express server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+let app = express();
+
+app.get('', (req, res) => {
+    res.send("Hello world!");
+})
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000");
+})
