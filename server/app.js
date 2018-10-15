3f272ff5420eda6946ba46648d92e434c2274164
Igor Amidzic <igoramidzic46@gmail.com>
2018-10-15 10:15:56 -0400

    master: Igor - commit

--- server/app.js
+++ server/app.js
@@ -1,47 +1,63 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var problemRouter = require("./routes/problemRouter");
-var mongoose = require("mongoose");
-var path = require("path");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var app = express();
+// var problemRouter = require("./routes/problemRouter");
+// var mongoose = require("mongoose");
+// var path = require("path");
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-const db = mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/algor-academy",
-  {
-    useNewUrlParser: true
-  }
-);
+// const db = mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/algor-academy",
+//   {
+//     useNewUrlParser: true
+//   }
+// );
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
 
-// CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+// // CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
-// Routes
-app.use("/api/problems", problemRouter);
+// // Routes
+// app.use("/api/problems", problemRouter);
 
-app.use(express.static(__dirname + "server/dist"));
+// app.use(express.static(__dirname + "server/dist"));
+// app.get("/*", function(req, res) {
+//   console.log(__dirname);
+
+//   res.sendFile(path.join(__dirname + "/dist/index.html"));
+// });
+
+// var server = app.listen(port, () => {
+//   console.log("app running on port ", server.address().port);
+// });
+
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist"));
+
 app.get("/*", function(req, res) {
-  console.log(__dirname);
-
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-var server = app.listen(port, () => {
-  console.log("app running on port ", server.address().port);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
