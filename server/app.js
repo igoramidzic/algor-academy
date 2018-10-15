d1399ddce746d8ff91fdee9c2596ec1a2ffe0276
Igor Amidzic <igoramidzic46@gmail.com>
2018-10-15 12:42:47 -0400

    master: Igor - commit

--- server/app.js
+++ server/app.js
@@ -45,19 +45,3 @@ app.get("/*", function(req, res) {
 var server = app.listen(port, () => {
   console.log("app running on port ", server.address().port);
 });
-
-// //Install express server
-// const express = require("express");
-// const path = require("path");
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + "/dist"));
-
-// app.get("/*", function(req, res) {
-//   res.sendFile(path.join(__dirname + "/dist/index.html"));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
--- src/app/services/problems.service.ts
+++ src/app/services/problems.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, SubscribableOrPromise, Subscribable } from "rxjs";
 import { Problem } from "../models/problem";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root"
@@ -16,19 +17,23 @@ export class ProblemsService {
   }
 
   getAllProblems(): Subscribable<any> {
-    return this.http.get("http://localhost:3000/api/problems");
+    return this.http.get(environment.api_url_base + "/api/problems");
   }
 
   getProblemById(id): Subscribable<any> {
-    return this.http.get("http://localhost:3000/api/problems/" + id);
+    return this.http.get(environment.api_url_base + "/api/problems/" + id);
   }
 
   updateProblemById(id, problem): Subscribable<any> {
     let headers = new HttpHeaders({
       "Content-Type": "application/json"
     });
-    return this.http.put("http://localhost:3000/api/problems/" + id, problem, {
-      headers
-    });
+    return this.http.put(
+      environment.api_url_base + "/api/problems/" + id,
+      problem,
+      {
+        headers
+      }
+    );
   }
 }
